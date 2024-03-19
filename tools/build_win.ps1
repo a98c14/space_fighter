6649107478f3f40327b6f3b46e852671760fe38e
a98c14 <selimyesilkaya@gmail.com>
2024-03-19 23:44:00 +0300

    workflow fix attempt #2

--- tools/build_win.ps1
+++ tools/build_win.ps1
@@ -5,12 +5,15 @@ foreach ($arg in $args) {
     $commands[$parts[0]] = if ($parts.Length -gt 1) { $parts[1] } else { 1 }
 }
 
+$root_directory = "$PSScriptRoot\.."
 if ($null -eq $commands["output"]) {
-    $commands["output"] = "$PSScriptRoot/../dist"
+    $commands["output"] = "$root_directory\dist\main.exe"
 }
 else {
-    $commands["output"] = "$PSScriptRoot/../$($commands["output"])"
+    $commands["output"] = "$root_directory\$($commands["output"])"
 }
+$output_directory = ([io.fileinfo]$($commands["output"])).DirectoryName
+$output_basename = ([io.fileinfo]$($commands["output"])).BaseName
 
 if ($null -eq $commands["version"]) {
     $commands["version"] = $(git describe --tags)
@@ -30,39 +33,45 @@ if ($commands["debug"]) {
     $compile_args += "/Z7";
     $compile_args += "/Od";
     $compile_args += "/DBUILD_DEBUG=1"
+    $commands["version"] = "$($commands["version"])-debug"
 }
 
-
 # --- Common Compile Flags ---------------------------------
+$compile_args += "/Fo$output_directory\$output_basename.obj"
 $compile_args += "/nologo"
-$compile_args += "/I..\opus\include"
-$compile_args += "/I..\opus\src"
+$compile_args += "/I.\opus\include"
+$compile_args += "/I.\opus\src"
 $compile_args += "/DSTB_IMAGE_IMPLEMENTATION"
 $compile_args += "/DVERSION_NUMBER=\""$($commands["version"])\"""
 
 # --- Libs -------------------------------------------------
-$compile_args += "..\opus\lib\soloud_x64_d.lib";
-$compile_args += "..\opus\lib\freetype.lib";
-$compile_args += "..\opus\lib\glfw3dll.lib";
-$compile_args += "..\opus\lib\glfw3.lib";
-$compile_args += "..\opus\lib\gl.obj";
+$compile_args += ".\opus\lib\soloud_x64_d.lib";
+$compile_args += ".\opus\lib\freetype.lib";
+$compile_args += ".\opus\lib\glfw3dll.lib";
+$compile_args += ".\opus\lib\glfw3.lib";
+$compile_args += ".\opus\lib\gl.obj";
 $compile_args += "opengl32.lib";
-
 
 # --- Set Link Flags --------------------------------------
 $link_args = @();
-$link_args += "/out:main.exe"
+$link_args += "/out:$output_directory\$output_basename.exe"
+if ($commands["debug"]) {
+    $link_args += "/pdb:$output_directory\$output_basename.pdb"
+}
 
 # --- Set Build Target ------------------------------------
 $source_file_path = ""
-if ($commands["game"]) { $source_file_path = "../src/main.c" }
+if ($commands["game"]) { $source_file_path = "$root_directory\src\main.c" }
 
-New-Item -ItemType Directory -Force -Path $($commands["output"]) | Out-Null;
-Push-Location $($commands["output"]) | Out-Null
+New-Item -ItemType Directory -Force -Path $output_directory | Out-Null;
+Push-Location $root_directory
+cl $compile_args $source_file_path /link $link_args;
+Pop-Location
+
+Push-Location $output_directory | Out-Null
 $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 Write-Host "[BUILD] target: $source_file_path";
-cl $compile_args $source_file_path /link $link_args;
 Write-Host "[BUILD] time: $(($stopwatch.ElapsedMilliseconds / 1000).ToString())s"
-if ($commands["run"]) { Invoke-Expression "$($commands["output"])\main.exe" }
+if ($commands["run"]) { Invoke-Expression "$output_directory\$output_basename.exe" }
 Pop-Location;
 
